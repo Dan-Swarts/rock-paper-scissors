c6dcc18628f0c91337bdcb3585456e8d8228d108
Daniel Swarts <swart179@umn.edu>
2024-08-15 20:50:00 -0500

    added develope

---
+++ Develop/script.js
@@ -0,0 +1,73 @@
+const playGame = function () {
+
+  // Create an object to track our statistics
+  const stats = statsObject//hint: look at the structure of the print out at the bottom of this function.
+
+  // Array of options for computer to pick from
+  const options = ['R', 'P', 'S'];
+
+  let keepPlaying = true;
+
+  // Loop until the user chooses to stop
+  while (keepPlaying) {
+
+    // Ask user for their choice
+    let userChoice = window.prompt('Enter R, P, or S:');
+
+    // If user pressed Cancel, immediately end function
+    if () {
+      return;
+    }
+
+    // Convert to uppercase to make comparisons easier
+    
+
+    // Check that the user made a valid choice
+    if () {
+      window.alert('Please enter a valid choice.');
+    } else {
+      //Check what the user input, track the statistics
+      
+
+      // Get random index from array of options
+      const index = Math.floor(Math.random() * options.length);
+      const computerChoice = options[index];
+
+      //Alert the user what the computer chose
+      
+
+      // If choices are the same, it's a tie
+      if (userChoice === computerChoice) {
+        // write the logic for a tie outcome
+
+        
+      } else if (
+        // Check every win condition for the player
+        ) {
+        stats.wins++;
+        window.alert('You win!');
+        
+      } else {
+        // If above conditions failed, assume player lost
+
+      }
+
+      // Ask user to play again, if not stop the loop
+      
+    }
+  }
+
+  // Print stats with line breaks
+  window.alert(`Stats:
+    Wins: ${stats.wins}
+    Losses: ${stats.losses}
+    Ties: ${stats.ties}
+
+    Your choices:
+    Rock: ${stats.count.rock}
+    Paper: ${stats.count.paper}
+    Scissors: ${stats.count.scissors}`);
+};
+
+// Run the game
+playGame();
